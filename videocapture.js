cc62edf8df6c62cfb9e36d1baef41a6df7ba3aa8
FiloCara <fili.cara@gmail.com>
2019-11-16 09:28:39 -0800

    add videocapture node, it not work well

---
+++ videocapture.js
@@ -0,0 +1,17 @@
+const cv = require('opencv4nodejs')
+
+module.exports = function(RED) {
+
+    function VideoCapture(config) {
+        RED.nodes.createNode(this, config);
+        var node  = this;
+        node.devicePort = config.devicePort;
+        const vCap = new cv.VideoCapture(node.devicePort);
+        node.on('input', function(msg) {
+            msg.payload = vCap.read();
+            msg.payload = cv.imencode('.jpg', msg.payload).toString('base64');
+            node.send(msg);
+        })
+    }
+    RED.nodes.registerType("videocapture",VideoCapture);
+};
