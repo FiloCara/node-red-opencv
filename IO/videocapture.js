542baad05ba20ae9833dff870e6fc195f34ac2c5
CARA <filippo.cara@plasticomnium.com>
2020-07-21 14:22:00 +0200

    add start stop functionality

--- IO/videocapture.js
+++ IO/videocapture.js
@@ -1,20 +1,45 @@
 const cv = require('opencv4nodejs')
 
 module.exports = function(RED) {
-
+    //TODO Implement a way to stop video streaming
     function VideoCapture(config) {
         RED.nodes.createNode(this, config);
         var node  = this;
         node.devicePort = config.devicePort;
-        const vCap = new cv.VideoCapture(node.devicePort);
-        node.on('input', function(msg) {
-            msg.payload = vCap.read();
-            msg.payload = cv.imencode('.jpg', msg.payload).toString('base64');
-            node.send(msg);
-        })
+        var vCap = null;
+        node.status({fill:"red",shape:"dot",text:"Not recording"});
 
+        node.on('input', function(msg) {
+            // If property start, then start recording
+            if (msg.hasOwnProperty('start') === true) {
+                node.status({fill:"yellow",shape:"dot",text:"Connecting"});
+                vCap = new cv.VideoCapture(node.devicePort);
+                vCap.read()
+                node.status({fill:"green",shape:"dot",text:"Recording"});
+            }
+            // If property reset, then close and release capture object
+            else if (msg.hasOwnProperty('reset') === true) {
+                
+                if (vCap !== null) {
+                    vCap.release()
+                    vCap = null;
+                    node.status({fill:"red",shape:"dot",text:"Not recording"});
+                }  
+            }
+            // If input and no 'start' or 'reset', then take a shot
+            else {
+                if (vCap !== null) {
+                    msg.payload = vCap.read();
+                    msg.payload = cv.imencode('.jpg', msg.payload).toString('base64');
+                    node.send(msg);
+                }
+            } 
+        })
         node.on('close', function() {
-            vCap.release()
+            if (vCap !== null) {
+                vCap.release()
+                node.status({fill:"red",shape:"dot",text:"Not recording"});
+            }
         })
     }
     RED.nodes.registerType("videocapture",VideoCapture);
