4a7004df801a01b1e30bb3882be56765d6ac610d
CARA <filippo.cara@plasticomnium.com>
2020-07-21 15:47:13 +0200

    Add resize node: dev

--- image_processing/resize.js
+++ image_processing/resize.js
@@ -5,19 +5,28 @@ module.exports = function(RED){
     function Resize(config){
         RED.nodes.createNode(this, config);
         var node  = this;
-        node.x = config.x
-        node.y = config.y
+        node.on('input', function(msg) {
+            // Decode img
+            decodedPic = cv.imdecode(Buffer.from(msg.payload,'base64'))
+
+            // Resize picture according to method choosen by the user
+
+            switch(config.method) {
+                case "Rescale":
+                    node.scaleFactor = config.scaleFactor;
+                    msg.payload = decodedPic.rescale(node.scaleFactor)
+                case "Resize":
+                    node.xSize = config.xSize;
+                    node.ySize = config.ySize;
+                    msg.payload = decodedPic.resize(node.xSize, node.ySize)
+                case "ResizeToMax":
+                    node.resizeFactor = config.resizeFactor;
+                    msg.payload = decodedPic.resizeToMax(node.resizeFactor)
+            }
+            // Encode
+            msg.payload = cv.imencode('.jpg', msg.payload).toString('base64');
+            node.send(msg);
+        });
     }
-    node.on('input', function(msg) {
-        // Decode img
-        decodedPic = cv.imdecode(Buffer.from(msg.payload,'base64'))
-        // Resize picture according to X and Y values provided by user
-        msg.payload = decodedPic.resize(node.x, node.y);
-        // Encode
-        msg.payload = cv.imencode('.jpg', msg.payload).toString('base64');
-        node.send(msg);
-    })
-
     RED.nodes.registerType("resize",Resize);
-
 }
