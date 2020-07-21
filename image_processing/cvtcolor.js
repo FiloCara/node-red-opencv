0ca678f457ca0d47c546d823310b549bf10ac3e3
CARA <filippo.cara@plasticomnium.com>
2020-07-21 17:42:03 +0200

    Solve bug

--- image_processing/cvtcolor.js
+++ image_processing/cvtcolor.js
@@ -5,28 +5,38 @@ module.exports = function(RED) {
     function CvtColor(config) {
         RED.nodes.createNode(this, config);
         var node  = this;
-        switch(config.colorspace) {
-            case "BGR2RGB":
-                node.colorspace = cv.COLOR_BGR2RGB
-            case "BGR2GRAY":
-                node.colorspace = cv.COLOR_BGR2GRAY
-            case "BGR2HLS":
-                node.colorspace = cv.COLOR_BGR2HLS
-            case "BGR2HSV":
-                node.colorspace = cv.COLOR_BGR2HSV
-            case "RGB2BGR":
-                node.colorspace = cv.COLOR_RGB2BGR
-            case "RGB2GRAY":
-                node.colorspace = cv.COLOR_RGB2GRAY
-            case "RGB2HLS":
-                node.colorspace = cv.COLOR_RGB2HLS
-            case "RGB2HSV":
-                node.colorspace = cv.COLOR_RGB2HSV
-            case "GRAY2BGR":
-                node.colorspace = cv.COLOR_GRAY2BGR
-            // Add others colorspace
-        }
+        node.colorspace = config.colorspace;
+
         node.on('input', function(msg) {
+            switch(node.colorspace) {
+                case "BGR2RGB":
+                    node.colorspace = cv.COLOR_BGR2RGB
+                    break;
+                case "BGR2GRAY":
+                    node.colorspace = cv.COLOR_BGR2GRAY
+                    break;
+                case "BGR2HLS":
+                    node.colorspace = cv.COLOR_BGR2HLS
+                    break;
+                case "BGR2HSV":
+                    node.colorspace = cv.COLOR_BGR2HSV
+                    break;
+                case "RGB2BGR":
+                    node.colorspace = cv.COLOR_RGB2BGR
+                    break;
+                case "RGB2GRAY":
+                    node.colorspace = cv.COLOR_RGB2GRAY
+                    break;
+                case "RGB2HLS":
+                    node.colorspace = cv.COLOR_RGB2HLS
+                    break;
+                case "RGB2HSV":
+                    node.colorspace = cv.COLOR_RGB2HSV
+                    break;
+                case "GRAY2BGR":
+                    node.colorspace = cv.COLOR_GRAY2BGR
+                    break;
+            }
             // Decode img
             decodedPic = cv.imdecode(Buffer.from(msg.payload,'base64'))
             // Change color space
